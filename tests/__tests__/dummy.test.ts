23d3aa0bcfe6379ca03f1e11a2363c23247b2546
batikan <nbatikansevil@gmail.com>
2025-11-25 16:49:34 +0300

    Issues

--- tests/__tests__/dummy.test.ts
+++ tests/__tests__/dummy.test.ts
@@ -6,7 +6,7 @@ const request = supertest(app)
 describe('Checking the health of the app', () => {
   test('It should respond with a health check message', async () => {
     const response = await request.get('/')
-    expect(response.body).toEqual({ 'health-check': 'OK: top level api working' })
+    expect(response.body).toEqual({ 'health-check': 'This is working!' })
     expect(response.status).toBe(200)
   })
 })
